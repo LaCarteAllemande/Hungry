c0400511e0d5f295cb1125fb1bd6d4ca1bbd8015
Nils <nmittel@protonmail.com>
2023-10-30 18:56:31 +0100

    added input box for recipies

--- scripts/animation.js
+++ scripts/animation.js
@@ -1,14 +1,8 @@
-var hiddenDivs = document.getElementsByClassName('hidden-div');
-Array.from(hiddenDivs).forEach(function (div) {
-    div.addEventListener('mouseover', function () {
-        setTimeout(function () {
-            div.classList.remove('delay-200');
-            console.log(1);
-        }, 500);
-    });
-    div.addEventListener('mouseleave', function () {
-        div.classList.remove('delay-500');
-        console.log(2);
-    });
-});
-//# sourceMappingURL=animation.js.map
+// Initialization for ES Users
+import {
+  Modal,
+  Ripple,
+  initTE,
+} from "tw-elements";
+
+initTE({ Modal, Ripple });
--- scripts/animation.ts
+++ scripts/animation.ts
@@ -1,21 +1,11 @@
-const hiddenDivs = document.getElementsByClassName('hidden-div') as HTMLCollectionOf<HTMLElement>;
+// Initialization for ES Users
+import {
+    Modal,
+    Ripple,
+    initTE,
+  } from "tw-elements";
   
-
-
-Array.from(hiddenDivs).forEach((div: Element) => {
-    div.addEventListener('mouseover', () => {
-        setTimeout(() => {
-            div.classList.remove('delay-200');
-            console.log(1)
-        }, 500); 
-      });
-    
-      div.addEventListener('mouseleave', () => {
-        div.classList.remove('delay-500');
-        console.log(2)
-    });
-  });
-
+  initTE({ Modal, Ripple });
   
 
 
